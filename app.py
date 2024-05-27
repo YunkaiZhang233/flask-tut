1f15a9f74f1834ca13434a7746d88bb4646ba942
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-05-27 22:13:42 +0100

    #3: Add a simple model to pass in data

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ from flask import Flask, render_template
 # create program object "app"
 app = Flask(__name__)
 
+# 3
+# simple data model
+all_todos = [
+    {
+        "id": "5b4a3a09-33f7-4724-b7da-63478652a38c",
+        "title": "Destroy the Death Start",
+        "description": "Identify and hit with a  perfect shot the only one weak spot on a space ship as big as whole planet while avoiding bad guys. NOTE TO SELF: The targeting system sucks. Use the force.",
+        "done": None,
+    },
+    {
+        "id": "d3b4ace0-bcf0-4d45-a7dc-84199f667a26",
+        "title": "Train with Yoda",
+        "description": "There is no try; only do.",
+        "done": None,
+    },
+]
+
+
+
 
 # create a function for processing request
 # officially called "view function"
@@ -21,11 +40,18 @@ app = Flask(__name__)
 # this function gets triggered
 # fetches return value
 # and displays the return value to the browser window
-
 # 2
 # flask will by default look for template files in `templates` folder
 def index():
     return render_template("index.html")
+
+
+@app.route("/todos", methods=["GET", "POST"])
+def todos():
+    return render_template("todos.html", todos=all_todos)
+
+
+
 
 
 if __name__ == '__main__':
